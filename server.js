5197b01dafe17443337b37a9c9b14cf7cf226cfe
madhusudan9332 <madhusudangautam003@gmail.com>
2024-06-11 18:13:24 +0530

    change api data and its syntax

--- server.js
+++ server.js
@@ -2,17 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-// const restaurantsFun = require("./restaurants");
-const cakes = require("./cakes");
-const chinese = require("./chinese");
-const cocktails = require("./cocktails");
-const desserts = require("./desserts");
-const pizzas = require("./pizzas");
-const vege = require("./vege");
-
-const menu = [...cakes, ...chinese, ...cocktails, ...desserts, ...pizzas, ...vege]
-
-
+const restaurants = require("./restaurants");
+const foodData = require("./data/data.json");
 
 const app = express();
 const PORT = process.env.PORT || 8880;
@@ -22,177 +13,68 @@ app.use(cors());
 // API endpoints
 
 app.get("/", (req, res) => {
-    res.json("Api Is Working successsfully");
-  });
-  app.get("/cakes", (req, res) => {
-    res.json(cakes);
-  });
-  app.get("/chinese", (req, res) => {
-    res.json(chinese);
-  });
-  app.get("/cocktails", (req, res) => {
-    res.json(cocktails);
-  });
-  app.get("/desserts", (req, res) => {
-    res.json(desserts);
-  });
-  app.get("/pizzas", (req, res) => {
-    res.json(pizzas);
-  });
-  app.get("/vege", (req, res) => {
-    res.json(vege);
-  });
-  
-  app.get("/menu", (req, res) => {
-    res.json(menu);
-  });
-  
-  // get item by name
-  
-  app.get("/menu/search/:query", (req, res) => {
-    let query = req.params.query || "";
-    query = query.toLowerCase();
-    console.log(query);
-    const results = menu.filter(
-      (item) =>
-        item.name.toLowerCase().includes(query)
-    );
-    res.json(results);
-  });
-  
-  // Add a new item
-  app.post("/items/:item", (req, res) => {
-    let item = req.params.itemtoLowerCase();
-    const { name, image, price, sizes = {}} = req.body;
-    if (!name || !image || !price) {
-      return res
-        .status(400)
-        .json({ message: "Name, image, and price are required" });
-    }
-    const newItem = {
-      id: cakes.length + 1,
-      name,
-      image,
-      price,
-      sizes : [sizes],
-      ...req.body
-    };
-    switch (item) {
-      case "cakes":
-        cakes.push(newItem);
-        break;
-      case "chinese":
-        chinese.push(newItem);
-        break;
-      case "cocktails":
-        cocktails.push(newItem);
-        break;
-      case "desserts":
-        desserts.push(newItem);
-        break;
-      case "pizzas":
-        pizzas.push(newItem);
-        break;
-      case "vege":
-        vege.push(newItem);
-        break;
-      default:
-        return res.status(400).json({ message: "Invalid item" });
-    }
-    menu = [...cakes, ...chinese, ...cocktails, ...desserts, ...pizzas, ...vege]
-    res.status(201).json(newItem);
-  });
+  res.json("Api Is Working successsfully");
+});
+app.get("/restaurants", (req, res) => {
+  res.json(restaurants);
+});
+app.get("/foodData", (req, res) => {
+  res.json(foodData);
+});
+app.get("/foodData/search/:query", (req, res) => {
+  const query = req.params.query.toLowerCase();
+  const filteredFoodData = foodData.filter((food) =>
+    food.name.toLowerCase().includes(query)
+  );
+  res.json(filteredFoodData);
+});
+// get item by name
 
-// PUT method to update an existing item
-app.put("/update/:item/:id", (req, res) => {
-  const item = req.params.item.toLowerCase();
-  const id = parseInt(req.params.id);
-
-  let itemList;
-  switch (item) {
-    case "cakes":
-      itemList = cakes;
-      break;
-    case "chinese":
-      itemList = chinese;
-      break;
-    case "cocktails":
-      itemList = cocktails;
-      break;
-    case "desserts":
-      itemList = desserts;
-      break;
-    case "pizzas":
-      itemList = pizzas;
-      break;
-    case "vege":
-      itemList = vege;
-      break;
-    default:
-      return res.status(400).json({ message: "Invalid item" });
-  }
-
-  const index = itemList.findIndex((item) => item.id === id);
+// Add a new item
+app.post("/items/:itemName", (req, res) => {
+  let itemName = req.params.itemName.toLowerCase();
+  const index = foodData.findIndex((item) => item.name === itemName);
   if (index === -1) {
     return res.status(404).json({ message: "Item not found" });
   }
-
-  const updatedItem = {
-    ...itemList[index], // Copy existing item
-    ...req.body, // Update with new data from request body
+  const { name, image, price, isVeg } = req.body;
+  if (!name || !image || !price) {
+    return res
+      .status(400)
+      .json({ message: "Name, image, and price are required" });
+  }
+  const newItem = {
+    id: cakes.length + 1,
+    name,
+    price,
+    image,
+    ...req.body,
   };
-
-  itemList[index] = updatedItem;
-
-  // Update the menu
-  menu = [...cakes, ...chinese, ...cocktails, ...desserts, ...pizzas, ...vege];
-
-  res.json(updatedItem);
+  if (isVeg) {
+    foodData[index].data["vegetarian"].push(newItem);
+  } else {
+    foodData[index].data["non vegetarian"].push(newItem);
+  }
+  res.status(201).json(newItem);
+  res.json(foodData[index]);
 });
-
 
 // PUT method to update all items of a specific type
-app.put("/update-all/:item", (req, res) => {
-  const item = req.params.item.toLowerCase();
-
-  let itemList;
-  switch (item) {
-    case "cakes":
-      itemList = cakes;
-      break;
-    case "chinese":
-      itemList = chinese;
-      break;
-    case "cocktails":
-      itemList = cocktails;
-      break;
-    case "desserts":
-      itemList = desserts;
-      break;
-    case "pizzas":
-      itemList = pizzas;
-      break;
-    case "vege":
-      itemList = vege;
-      break;
-    default:
-      return res.status(400).json({ message: "Invalid item" });
+app.put("/update-id/:itemName", (req, res) => {
+  let itemName = req.params.itemName.toLowerCase();
+  const index = foodData.findIndex((item) => item.name === itemName);
+  if (index === -1) {
+    return res.status(404).json({ message: "Item not found" });
   }
-
-  // Update all items of the specified type
-  itemList.forEach((item, index) => {
-    itemList[index] = {
-      ...item,
-      ...req.body, // Update with new data from request body
-    };
+  foodData[index].data["vegetarian"] = foodData[index].data["vegetarian"].map((vegItem, vegIndex) => {
+    return { ...vegItem, id: vegIndex + 1 };
   });
+  foodData[index].data["non vegetarian"] = foodData[index].data["non vegetarian"].map((nonVegItem, nonVegIndex) => {
+    return { ...nonVegItem, id: nonVegIndex + 1 };
+  });
+  res.json(foodData[index]);
 
-  // Update the menu
-  menu = [...cakes, ...chinese, ...cocktails, ...desserts, ...pizzas, ...vege];
-
-  res.json(itemList); // Return updated items
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
